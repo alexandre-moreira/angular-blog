5f5c3aeec2059fac8acfd6f4658bfd109742eb06
alsouza <alexandre.souza@foursys.com.br>
2025-02-26 14:54:48 -0300

    feat: Menu Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
