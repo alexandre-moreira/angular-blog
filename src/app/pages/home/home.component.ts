a412d6fb1adfeaa3eb26f2273787d9559f5f0442
alsouza <alexandre.souza@foursys.com.br>
2025-02-27 14:30:03 -0300

    feat: created component menu-bar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, BigCardComponent],
+  imports: [MenuTitleComponent, BigCardComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
